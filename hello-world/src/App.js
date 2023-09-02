efdd7c4b687b2b5848a38c5d316f5671c06a2b93
Seth <Sethhatch2003@gmail.com>
2023-09-02 00:50:50 -0600

    Added hello-world
    Sorry for late uploads. Was having issues with VSCode

---
+++ hello-world/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://www.freeiconspng.com/thumbs/earth-png/planet-earth-png-8.png" className="App-logo" alt="logo" />
+        <p>
+          Hello World.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/alexhovick/ShopZoneCS350"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ShopZone github repository
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
