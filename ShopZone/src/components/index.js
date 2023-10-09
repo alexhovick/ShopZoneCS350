8e4e6292503f03967477a4f21b81cd6aff671f85
alexhovick <143450573+alexhovick@users.noreply.github.com>
2023-10-09 14:58:47 -0600

    Update index.js

--- ShopZone/src/components/index.js
+++ ShopZone/src/components/index.js
@@ -1,13 +1,11 @@
 import Header from './Header';
 import ProductCard from './ProductCard';
 import Error from './Error';
-import Loader from './Loader';
-import SearchBar from './SearchBar';
+import Loader from './Loader'
 
 export {
   Header,
   ProductCard,
   Error,
   Loader,
-  SearchBar,
 };
