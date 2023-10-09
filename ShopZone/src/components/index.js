a0e0248f5a4673569281ec332885c0994b9443c2
dale <scuffedleaf@gmail.com>
2023-10-09 14:49:34 -0600

    export searchbar

--- ShopZone/src/components/index.js
+++ ShopZone/src/components/index.js
@@ -2,10 +2,12 @@ import Header from './Header';
 import ProductCard from './ProductCard';
 import Error from './Error';
 import Loader from './Loader'
+import SearchBar from './SearchBar';
 
 export {
   Header,
   ProductCard,
   Error,
   Loader,
+  SearchBar,
 };
