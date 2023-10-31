0958d4cbf5fe84426d839c339689322bb408ec0f
dale <scuffedleaf@gmail.com>
2023-10-30 19:25:15 -0600

    starting stripe integration

--- ShopZone/src/redux/services/Stripe.js
+++ ShopZone/src/redux/services/Stripe.js
@@ -1,1 +1,5 @@
 import Stripe from 'stripe';
+import { NEXT_PUBLIC_PUBLISHABLE_KEY, NEXT_PUBLIC_STRIPE_SECRET_KEY } from './config.js';
+
+
+const stripe = new Stripe(NEXT_PUBLIC_STRIPE_SECRET_KEY);
