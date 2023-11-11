d743f3b5cef45e3c6f0271131e3f23160057b30d
alexhovick8 <62569769+alexhovick8@users.noreply.github.com>
2023-11-10 19:50:11 -0700

    lets try this one more time

--- ShopZone/src/index.jsx
+++ ShopZone/src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom'; // Corrected import statement
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
@@ -7,7 +7,7 @@ import './index.css';
 import App from './App';
 import { store } from './redux/store';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+render(
   <React.StrictMode>
     <Provider store={store}>
       <Router basename=''>
@@ -15,4 +15,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </Router>
     </Provider>
   </React.StrictMode>,
+  document.getElementById('root')
 );
